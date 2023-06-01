69e9cf88cb32d02afe88098794d2471b8190ca2a
Thomas Amlee <thomasamlee@Thomass-MacBook-Air.local>
2023-06-01 12:57:30 -0500

    Deploy

--- _next/static/chunks/app/about/page-378261c5041d3ac5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{6625:function(n,e,u){Promise.resolve().then(u.t.bind(u,8920,23))}},function(n){n.O(0,[920,667,488,744],function(){return n(n.s=6625)}),_N_E=n.O()}]);
