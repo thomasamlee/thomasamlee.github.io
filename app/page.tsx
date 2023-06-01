8a5830c30389839fa54c55abca55842bab3df620
Thomas Amlee <thomasamlee@Thomass-MacBook-Air.local>
2023-06-01 15:25:47 -0500

    placeholder text

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 export default function Home() {
   return (
-    <>
-      <div>
-        <img
-          className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          alt=""
-        />
-      </div>
-      <h1 className="text-3xl font-bold underline">Hello!</h1>
-    </>
+    <div>
+      <h1 className="text-lg">Thomas Amlee</h1>
+      <p className="text-sm">Coming Soon...</p>
+    </div>
   );
 }
