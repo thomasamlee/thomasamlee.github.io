56fed7446cc3e652bc48ae7af8e66372099555ce
Thomas Amlee <thomasamlee@Thomass-MacBook-Air.local>
2023-06-01 12:57:11 -0500

    add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import HelloWorld from "./hello.mdx";
 
 export default function Home() {
-  return <HelloWorld />;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <HelloWorld />
+    </>
+  );
 }
